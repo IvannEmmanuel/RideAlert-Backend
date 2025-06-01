77e67898b6e727ccbfffe80ff4123b130d46dbbd
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-06-01 20:54:41 +0800

    refactor: added a routes for vehicle

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.routes import user
+from app.routes import vehicle
 
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(vehicle.router)
 # Include other routers as needed
 
 @app.get("/")
