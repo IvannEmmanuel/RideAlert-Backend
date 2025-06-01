d56bb4ca4809eab6af95864d5caba7d010ce4ad7
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-06-01 13:02:29 +0800

    added database

---
+++ app/database.py
@@ -0,0 +1,19 @@
+# app/database.py
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+
+try:
+    client.admin.command("ping")
+    print("✅ Connected to MongoDB")
+except Exception as e:
+    print("❌ MongoDB connection error:", e)
+
+db = client["ridealertDB"]
+user_collection = db["users"]
