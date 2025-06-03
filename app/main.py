b67058495afa46c50f62755147ab3c621cfbe0cc
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-06-03 17:22:41 +0800

    refactor: added a middlware

--- app/main.py
+++ app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from app.routes import user
 from app.routes import vehicle
 from app.routes.websockets import ws_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins, adjust as needed
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods, adjust as needed
+    allow_headers=["*"],  # Allow all headers, adjust as needed
+)
 
 app.include_router(user.router)
 app.include_router(vehicle.router)
