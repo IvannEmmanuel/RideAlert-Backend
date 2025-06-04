aaa56b67a72badf67066f07c23450da302db0733
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-06-04 18:45:49 +0800

    refactor: added a new role

--- app/dependencies/roles.py
+++ app/dependencies/roles.py
@@ -10,3 +10,11 @@ def user_required(current_user: dict = Depends(get_current_user)):
     if current_user.get("role") != "user":
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Users only")
     return current_user
+
+def user_or_admin_required(current_user: dict = Depends(get_current_user)):
+    if current_user.get("role") not in ("user", "admin"):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You do not have permission to access this resource."
+        )
+    return current_user
