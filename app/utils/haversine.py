1f636017b43b55f7cb29c57b177da15f767f8d22
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-06-05 20:49:45 +0800

    feat: calculates the vehicle and users location

---
+++ app/utils/haversine.py
@@ -0,0 +1,13 @@
+import math
+
+def haversine_code(lat1, lon1, lat2, lon2):
+    R = 6371000  # Radius of Earth in meters
+    phi1 = math.radians(lat1)
+    phi2 = math.radians(lat2)
+    dphi = math.radians(lat2 - lat1)
+    dlam = math.radians(lon2 - lon1)
+
+    a = math.sin(dphi / 2) ** 2 + math.cos(phi1) * math.cos(phi2) * math.sin(dlam / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    return distance
