6010d50800530b5a1d96de3dab63254baaacc402
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-08-24 13:16:04 +0800

    feature: added a count for vehicle

--- app/routes/vehicle.py
+++ app/routes/vehicle.py
@@ -92,3 +92,11 @@ def track_all_vehicles(current_user: dict = Depends(user_or_admin_required)):
     if not vehicles:
         raise HTTPException(status_code=404, detail="No vehicles with valid locations found")
     return vehicles
+
+@router.get("/count")
+def count_vehicles(current_user: dict = Depends(user_or_admin_required)):
+    try:
+        total = vehicle_collection.count_documents({})
+        return {"count": total}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error counting vehicles: {str(e)}")
