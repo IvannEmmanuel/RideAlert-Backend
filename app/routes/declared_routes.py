5b258ea68ce250fb551d80c4823ea28bc83e2b90
Axel Paredes <paredes.axel456@gmail.com>
2025-10-10 21:09:27 +0800

    baliktad

--- app/routes/declared_routes.py
+++ app/routes/declared_routes.py
@@ -1,11 +1,32 @@
-
-
-from fastapi import APIRouter, HTTPException, UploadFile, File, Form, Depends
-from app.database import get_declared_routes_collection
-from app.dependencies.roles import super_and_admin_required
 import json
+from app.database import get_declared_routes_collection
+from fastapi import APIRouter, HTTPException, UploadFile, File, Form, Depends
+from fastapi import Path
+from app.dependencies.roles import super_and_admin_required
+
 
 router = APIRouter(prefix="/declared_routes", tags=["Declared Routes"])
+
+
+@router.patch("/{route_id}/route-geojson", response_model=dict)
+async def update_route_geojson(
+    route_id: str = Path(..., description="Declared route ID"),
+    route_geojson: UploadFile = File(...),
+    current_user: dict = Depends(super_and_admin_required)
+):
+    try:
+        geojson_content = await route_geojson.read()
+        route_geojson_dict = json.loads(geojson_content)
+        result = get_declared_routes_collection.update_one(
+            {"_id": route_id},
+            {"$set": {"route_geojson": route_geojson_dict}}
+        )
+        if result.matched_count == 0:
+            raise HTTPException(
+                status_code=404, detail="Declared route not found")
+        return {"updated": True}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.post("/upload", response_model=dict)
